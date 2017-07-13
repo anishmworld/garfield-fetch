01723e5eeb2f99d78f7adc32c011aaba5d45f0a9
Anish Majumder <anishmworld@users.noreply.github.com>
2017-07-13 14:30:04 +0530

    A bit more elegant exception handling

--- garfield.ps1
+++ garfield.ps1
@@ -39,32 +39,41 @@ $start = ( ParseDateStr $DateStart )
 $finish = ( ParseDateStr $DateFinish )
 $today = (Get-Date).toString("yyyy-MM-dd")
 
-if ($start.toString("yyyy-MM-dd") -gt $finish.toString("yyyy-MM-dd"))
+if ($start.toString("yyyy-MM-dd") -le $finish.toString("yyyy-MM-dd"))
+{
+	if ($start.toString("yyyy-MM-dd") -lt '1978-06-19')
+	{
+		pause "The earliest comic is on 19th June, 1978. `nEnter the dates on or after that. `nPress any key to exit"
+		exit
+	}
+
+	if ($start.toString("yyyy-MM-dd") -gt $today)
+	{
+		pause "The dates are in the future. `nTardis doesn't support this script... Yet. `nPress any key to exit"
+		exit
+	}
+	
+	if ($finish.toString("yyyy-MM-dd") -gt $today)
+	{
+		pause "The Finish date is in the future. `nTardis doesn't support this script... Yet. `nPress any key to exit"
+		exit
+	}
+
+	$URLstub = 'https://d1ejxu6vysztl5.cloudfront.net/comics/garfield'
+	$delta = New-TimeSpan -Days 1
+
+	for ($d = $start; $d -le $finish; $d +=$delta)
+	{
+		$date = $d.toString("yyyy-MM-dd")
+		$year = $d.ToString("yyyy")
+		$file = @($date, 'gif') -join '.'
+		$URL = @($URLstub, $year, $file) -join '/'
+		Write-Host 'Downloading comic on: ' $date
+		Invoke-WebRequest -Uri $URL -OutFile $file
+	}
+}
+else
 {
 	pause "The Start Date cannot be after End Date. `nPress any key to exit"
 	exit
-}
-
-if ($start.toString("yyyy-MM-dd") -lt '1978-06-19' -or $finish.toString("yyyy-MM-dd") -lt '1978-06-19')
-{
-	pause "The earliest comic is on 19th June, 1978. `nEnter the dates on or after that. `nPress any key to exit"
-	exit
-}
-
-if ($start.toString("yyyy-MM-dd") -gt $today -or $finish.toString("yyyy-MM-dd") -gt $today)
-{
-	pause "The date(s) is(are) in the future. `nTardis doesn't support this script... Yet. `nPress any key to exit"
-	exit
-}
-$URLstub = 'https://d1ejxu6vysztl5.cloudfront.net/comics/garfield'
-$delta = New-TimeSpan -Days 1
-
-for ($d = $start; $d -le $finish; $d +=$delta)
-{
-    $date = $d.toString("yyyy-MM-dd")
-    $year = $d.ToString("yyyy")
-    $file = @($date, 'gif') -join '.'
-    $URL = @($URLstub, $year, $file) -join '/'
-	Write-Host 'Downloading comic on: ' $date
-    Invoke-WebRequest -Uri $URL -OutFile $file
 }
